62beb7d1efd302bdca9d07000c54c33e69206c92
Rodeffs <guideandplaygamespilex24@gmail.com>
2023-10-03 23:40:02 +0200

    work started on lab2 OOP

---
+++ OOP/lab2/Array3D.h
@@ -0,0 +1,88 @@
+#pragma once
+
+/* Задание, орфография от автора:
+
+Создаем класс шаблонный Arry3d<> который хранит одномерный массив, гно авдыает наружу его как трезхмерные. Переопрделеить операти=оры индексации [][][], <<
+Сдлать методы GetValue0(int i) ->[][] GeValue1(int j) ->[][]  GetValues2(int k) -> [][]
+GetValues01(int i, int j) -> []
+GetValues02(int i, int k) ->[]
+GetValues12(in j, int k) -> []
+и аналогичные методы для SetValues
+Сделать статический метод CrteaeFill для создания массива и присваивания всех элементов одному значению
+
+*/
+
+
+// ВАЖНО: см. explanation.png для пояснения того, как из 3-мерного массива сделать одномерный и как мы обходим список по индексам. Стоит отметить, что в том списке, что указан в примере, могут быть абсолютно разные элементы, важен лишь их индекс и размерность каждой координаты
+
+namespace Arrays {
+
+	template <class T>
+	class Array3D {
+	private:
+		int m_dim0{}, m_dim1{}, m_dim2{};  // размерности по i, j и k соответственно
+		int N{m_dim0*m_dim1*m_dim2};  // величина массива, чтобы не пришлось пересчитывать
+		T* array3d[];
+
+	public:
+		Array3D(int dim0, int dim1, int dim2) : m_dim0{dim0}, m_dim1{dim1}, m_dim2{dim2} {
+			T array[m_dim0*m_dim1*m_dim2];
+			*array3d = array;	
+		}
+
+		T getValues0(int i) {  // возвращает значения в i строке (плоскость Oyz)
+			T returnArr[m_dim2][m_dim1];
+			int y{0};
+			for (int k = i; k < N; k += m_dim0 * m_dim1) {
+				for (int j = 0; j < m_dim1; j++)
+					returnArr[y][j] = array3d[k+j];
+				y++;
+			}
+			return returnArr;
+		}
+
+		T getValues1(int j) {  // возвращает значения в j столбце (плоскость Oxz)
+			T returnArr[m_dim2][m_dim0];
+			int i{j};
+			for (int k = 0; k < m_dim2; k++)
+				for (int x = 0; x < m_dim0; x++) {
+					returnArr[k][x] = array3d[i];
+					i += m_dim1;
+				}
+			return returnArr;
+		}
+		
+		T getValues2(int k) { // возвращает значения в k слое (плоскость Oxy)
+			T returnArr[m_dim1][m_dim0];
+			int t{0};
+			for (int j = 0; j < m_dim1; j++)
+				for (int i = 0; i < m_dim0; i++) {
+					returnArr[j][i] = array3d[k * m_dim0 * m_dim1 + t];
+					t++;
+				}
+			return returnArr;
+		}
+
+		T getValues01(int i, int j) {  // возвращает значения в оси аппликат (для k)
+			T returnArr[m_dim2];
+			
+		}
+
+		T getValues02(int i, int k);
+
+		T getValues12(int j, int k);
+
+		void setValues0(int *arr12);
+			
+		void setValues1(int *arr02);
+		
+		void setValues2(int *arr01);
+
+		void setValues01(int *arr, int j);
+
+		void setValues02(int i, int k);
+
+		void setValues12(int j, int k);
+
+	};
+}
---
+++ OOP/lab2/templates.cpp
@@ -0,0 +1,3 @@
+#include "Array3D.cpp"
+
+template int Arrays::createArray<int>(int dim0, int dim1, int dim2);
