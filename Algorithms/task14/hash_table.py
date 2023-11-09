26251b968620e07e314a16a965bdc1b116535e71
Rodeffs <guideandplaygamespilex24@gmail.com>
2023-11-09 20:59:33 +0200

    task14 slightly updated, more comments added

--- Algorithms/task14/hash_table.py
+++ Algorithms/task14/hash_table.py
@@ -1,10 +1,9 @@
 def hash_table(arr, alphabet):
-    hashed = [[] for i in alphabet]
+    hashed = [[] for i in alphabet]  # создаём массив для хеша, размер которого равен кол-ву символов в алфавите
     for i in range(0, len(arr)):
-        first_letter = arr[i][0]
         for j in range(0, len(alphabet)):
-            if first_letter == alphabet[j]:
-                hashed[j].append(arr[i])
+            if arr[i][0] == alphabet[j]:  # затем заносим в хеш элементы из файла по принципу: индекс элемента равен первой букве слова (т.е. для A индекс 0, для a - 1 и т.д.)
+                hashed[j].append(arr[i])  # т.к. это хеш функция со списками, то слова с такой же первой буквой просто добавляем к остальным
                 break
 
     return hashed
@@ -13,15 +12,13 @@ def hash_table(arr, alphabet):
 if __name__ == "__main__":
 
     with open("input.txt", "r") as input_file:  # открываем файл вводных данных в режиме только для чтения
-        inputs = [i.strip(",").strip(".").strip(":").strip(";").strip("!").strip("?") for i in input_file.read().split()]
+        inputs = [i.strip(",.:;!?") for i in input_file.read().split()]
     
     alphabet = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzАаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщъыьЭэЮюЯя"
     hashed = hash_table(inputs, alphabet)
 
     with open("output.txt", "a") as output_file:  # открываем (или создаём, если его нет) файл для вывода таблицы в режиме добавления
-        j = 1
         for i in range(0, len(hashed)):
             if hashed[i]:
-                output_file.write(f"{j} {alphabet[i]} | {hashed[i]}\n")
+                output_file.write(f"{i+1} {alphabet[i]} | {hashed[i]}\n")
                 output_file.write("----------------------------------------------------\n")
-                j += 1
