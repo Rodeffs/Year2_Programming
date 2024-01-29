d45c3b67d9845009b36c92434e059c57efbf7e62
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-01-29 11:06:26 +0200

    lab5 work started

---
+++ Algorithms/4th_Semester/lab5/strongly_connected.py
@@ -0,0 +1,10 @@
+def main():
+
+    with open("input.txt") as mat:
+        graph = [i.split() for i in mat.readlines()]
+    for i in range(0, len(graph)):
+        graph[i] = [int(j) for j in graph[i]]
+
+
+if __name__ == "__main__":
+    main()
