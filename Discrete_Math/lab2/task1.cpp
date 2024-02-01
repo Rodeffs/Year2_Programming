c1a3ec39d4eec9d3a8ec050e3f921e46e327fb25
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-02-01 17:58:59 +0200

    comments added

--- Discrete_Math/lab2/task1.cpp
+++ Discrete_Math/lab2/task1.cpp
@@ -6,11 +6,11 @@
 using std::vector;
 using std::string;
 
-bool isNumIn(int num, vector<int> &numbers) {
+bool isNumIn(int num, vector<int> &numbers) {  // поиск индекса буквы из основного слова в нынешнем
 	return std::find(numbers.begin(), numbers.end(), num) != numbers.end();
 }
 
-bool isCharIn(char letter, vector<char> &letters) {
+bool isCharIn(char letter, vector<char> &letters) {  // проверка на то, была ли эта буква на данной итерации цикла
 	return std::find(letters.begin(), letters.end(), letter) != letters.end();
 }
 
@@ -19,16 +19,16 @@ void makeword(string& base, int length, int* wordcounter, vector<int> &new_word)
 
 	for (int i = 0; i < base.length(); i++)
 		if (!isNumIn(i, new_word) && !isCharIn(base[i], used_letters)) {
-			new_word.push_back(i);
-			used_letters.push_back(base[i]);
+			new_word.push_back(i);  // добавляем букву в слово, потом по рекурсии добавим другую, которой ещё не было
+			used_letters.push_back(base[i]);  // чтобы цикл проходился только по УНИКАЛЬНЫМ буквам, т.е. по тем, которых не было в предыдущих итерациях ЦИКЛА
 
 			if (new_word.size() == length) {
 				(*wordcounter)++;
 
+				// Для вывода всех слов, это необязательно для работы программы:
 				string actual_word = "";
 				for (int j = 0; j < new_word.size(); j++)
 					actual_word += base[new_word[j]];
-
 				std::cout << actual_word << std::endl;
 			}
 			else
