4010b1727dbd1450c9add14c24cf0b7fdbea9818
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-02-01 19:26:30 +0200

    small refactoring

--- Discrete_Math/lab2/task2.py
+++ Discrete_Math/lab2/task2.py
@@ -5,18 +5,18 @@ def a(n, vals):
         return -7
 
     if not vals[n]:
-        vals[n] =  5*a(n-1, vals) + 6*a(n-2, vals)
+        vals[n] = 5*a(n-1, vals) + 6*a(n-2, vals)
 
     return vals[n]
 
 
 def main():
-    values = [0 for i in range(0, 101)]
+    n = int(input("Введите n для нахождения a(n):\n"))
 
+    values = [0 for i in range(0, n+1)]
     values[0] = 1
     values[1] = -7
 
-    n = int(input("Введите n для нахождения a(n):\n"))
     print(f"a({n}) =", a(n, values))
 
 
