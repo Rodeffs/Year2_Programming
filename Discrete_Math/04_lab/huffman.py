38e80c3966bbc16b4b155f9fb4b4408987cd2812
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-02-20 11:05:50 +0200

    pair encoding added

--- Discrete_Math/04_lab/huffman.py
+++ Discrete_Math/04_lab/huffman.py
@@ -46,12 +46,31 @@ def shannon(stat, size):  # формула Шеннона
     return shan
 
 
+def encode_huffman(stat, char_groups_codes, coded):
+    characters = list(stat.keys())
+    char_group = list(char_groups_codes.keys())
+    for i in range(0, len(characters)):
+        cur_char = characters[i]
+        coded[cur_char] = ""
+        for j in range(0, len(char_group)):
+            if cur_char in char_group[j]:
+                coded[cur_char] = char_groups_codes[char_group[j]] + coded[cur_char]
+
+
+def count(stat):
+    char_count = 0
+    values = list(stat.values())
+    for i in range(0, len(values)):
+        char_count += values[i]
+    return char_count
+
+
 def main():
     with open("sample.txt", "r") as f:
         file = f.read()
 
-    stat, char_groups_codes, coded, stat2 = {}, {}, {}, {}
-    char_count, new_size = 0, 0
+    stat, char_groups_codes, coded, stat2,  char_pairs_codes, coded2 = {}, {}, {}, {}, {}, {}
+    new_size, new_size2 = 0, 0
 
     # Статистический анализ:
     for i in range(0, len(file)):
@@ -63,9 +82,7 @@ def main():
     stat.pop("\n")
 
     # Подсчёт количества символов:
-    values = list(stat.values())
-    for i in range(0, len(values)):
-        char_count += values[i]
+    char_count = count(stat)
     print("1) Изначальное количество символов в тексте:", char_count)
 
     # Сортировка по частоте:
@@ -90,23 +107,22 @@ def main():
     stat2 = dict(sorted(stat2.items(), key=lambda item: item[1]))
     print("3) Количество вхождений пар:\n", stat2)
 
+    # Кодировка каждого символа (если символ входит в пару, то добавляем слева код этой пары):
     stat_copy = dict(stat)  # нужно вызвать именно конструктор копирования
     huffman(stat_copy, char_groups_codes)
+    encode_huffman(stat, char_groups_codes, coded)
+    print("4.1) Код каждого символа:\n", coded)
 
-    # Кодировка каждого символа (если символ входит в пару, то добавляем слева код этой пары):
-    characters = list(stat.keys())
-    char_group = list(char_groups_codes.keys())
-    for i in range(0, len(characters)):
-        cur_char = characters[i]
-        coded[cur_char] = ""
-        for j in range(0, len(char_group)):
-            if cur_char in char_group[j]:
-                coded[cur_char] = char_groups_codes[char_group[j]] + coded[cur_char]
-    print("4) Код каждого символа:\n", coded)
+    # Кодировка пар:
+    stat2_copy = dict(stat2)
+    huffman(stat2_copy, char_pairs_codes)
+    encode_huffman(stat2, char_pairs_codes, coded2)
+    print("4.2) Код пар:\n", coded2)
 
     # После сжатия:
     size = char_count * 5
     avg = average(stat, coded, char_count)
+    characters = list(stat.keys())
     for i in range(0, len(characters)):
         new_size += stat[characters[i]] * len(coded[characters[i]])
     print(f"5) Изначальный размер текста был {size} бит, после кодировки стал {new_size} бит")
@@ -116,6 +132,20 @@ def main():
     shan_val = shannon(stat, char_count)
     print("9) Количество информации по Шеннону равно:", shan_val)
     print("10) Отношение среднего размера символа кодов Хаффмана и количества информации из формулы Шеннона:", 1 - shan_val / avg)
+
+    # Для пар:
+    print("\nДля пар:")
+    pair_count = count(stat2)
+    avg2 = average(stat2, coded2, pair_count)
+    pairs = list(stat2.keys())
+    for i in range(0, len(pairs)):
+        new_size2 += stat2[pairs[i]] * len(coded2[pairs[i]])
+    print(f"11) Изначальный размер текста был {size} бит, после кодировки стал {new_size2} бит")
+    print(f"12) Степень сжатия: {new_size2/size}\n13) Коэф. сжатия: {size/new_size2}\n14) Средняя длина слова: {avg2}")
+
+    shan_val2 = shannon(stat2, pair_count)
+    print("15) Количество информации по Шеннону равно:", shan_val2)
+    print("16) Отношение среднего размера символа кодов Хаффмана и количества информации из формулы Шеннона:", 1 - shan_val2 / avg2)
 
 
 if __name__ == '__main__':
