ee233a23aba50462fb11b768357bef39f0ab2f83
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-03-05 19:38:03 +0200

    step 2

--- WEB/02_lab/php_basics.php
+++ WEB/02_lab/php_basics.php
@@ -7,14 +7,39 @@ $very_bad_unclear_name = "15 chicken wings";
 
 // 1 пункт
 
-$order = &$very_bad_unclear_name;  # ссылки как в C
+// Ссылка как в C и C++
+$order = &$very_bad_unclear_name;
 
-$order.=" I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda\n";
+// Конкатенация через .
+$order.=", two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda";
   
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
 
+echo "\n";
+
 
 // 2 пункт
 
+$x = 420;
 
+echo "int = $x";
+
+echo "\n";
+
+$f = 19.8588925893205832953839;
+
+echo "float = $f\n";
+
+$g = 96 / 8;
+
+echo "twelve = $g\n";
+
+$last_month = 1187.23;
+
+$this_month = 1089.98;
+
+echo $last_month - $this_month, "\n";
+
+
+// 3 пункт
