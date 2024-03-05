af575f7dcb04edcfc7604dd4da93d76a978f091b
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-03-05 19:24:31 +0200

    lab 02 step 1 done

---
+++ WEB/02_lab/php_basics.php
@@ -0,0 +1,20 @@
+<?php
+/* Imagine a lot of code here */ 
+$very_bad_unclear_name = "15 chicken wings";
+
+// Write your code here:
+
+
+// 1 пункт
+
+$order = &$very_bad_unclear_name;  # ссылки как в C
+
+$order.=" I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda\n";
+  
+// Don't change the line below
+echo "\nYour order is: $very_bad_unclear_name.";
+
+
+// 2 пункт
+
+
