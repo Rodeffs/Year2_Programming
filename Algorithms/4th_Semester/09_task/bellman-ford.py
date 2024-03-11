83f2d7087f8f130b8e56e00aeaac63eecd03c2a3
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-03-11 10:17:12 +0200

    task 09 algos done

--- Algorithms/4th_Semester/09_task/bellman-ford.py
+++ Algorithms/4th_Semester/09_task/bellman-ford.py
@@ -4,12 +4,12 @@ def main():
     for i in range(0, len(graph)):
         graph[i] = [int(j) for j in graph[i]]
 
-    total_nodes = len(graph[i])
-    startnode = int(input(f"В графе {total_nodes} вершин, введите от какой вершины считать кратчайшие пути: "))
+    v = len(graph[i])
+    start = int(input(f"В графе {v} вершин, введите стартовую вершину: "))
     distance = []
 
-    for i in range(0, total_nodes):
-        if i == startnode:
+    for i in range(0, v):
+        if i == start:
             distance.append(0)
         else:
             distance.append(float('inf'))  # бесконечность в питоне
@@ -18,17 +18,17 @@ def main():
     # расслабляя веса вершин (т.е. уменьшать их веса, если нашёлся путь короче)
     # Отличительная черта от алгоритма Дейкстры - возможность работы с
     # отрицательными весами, но за счёт скорости работы алгоритма
-    for x in range(0, total_nodes-1):
-        for i in range(0, total_nodes):
-            for j in range(0, total_nodes):
+    for x in range(0, v-1):
+        for i in range(0, v):
+            for j in range(0, v):
                 path = graph[i][j]
                 if path != 0:  # т.к. расстояние может быть < 0 а это False
                     if distance[j] > distance[i] + path:
                         distance[j] = distance[i] + path
 
-    for i in range(0, total_nodes):
-        if i != startnode:
-            print(f"Кратчайшее расстояние от {startnode} до {i} равно {distance[i]}")
+    for i in range(0, v):
+        if i != start:
+            print(f"Кратчайшее расстояние от {start} до {i} = {distance[i]}")
 
 
 if __name__ == '__main__':
