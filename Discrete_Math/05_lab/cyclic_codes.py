a0e4776af39c772f04ee2396e52f0d1e7be08339
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-03-19 00:46:04 +0200

    step 1 done

--- Discrete_Math/05_lab/cyclic_codes.py
+++ Discrete_Math/05_lab/cyclic_codes.py
@@ -1,3 +1,25 @@
+import math
+
+
+def bit_count(x):
+    # Считаю количество битов через логарифм по осн. 2
+    # В случае, если число - степень 2, то добавляю 1 к ответу
+    is_power_of_2 = (x & (x-1)) == 0
+    return math.ceil(math.log(x, 2)) + is_power_of_2
+
+
+def polynom_remain(f, g):  # деление многочленов
+    r = f
+    do_once = True  # костыль, т.к. нужно минимум 1 раз поделить
+
+    while bit_count(g) <= bit_count(r) or do_once:
+        do_once = False
+        div = g << (bit_count(r) - bit_count(g))
+        r = r ^ div  # операция XOR, т.е. модуль от 2 суммы битов
+
+    return r
+
+
 def main():
     n = 15  # общее число элементов
     m = 5  # информационные элементы
@@ -24,22 +46,23 @@ def main():
         row = ""
         for j in range(0, n):
             row += (str(matrix[i][j])+" ")
-        print(row, "\n")
+        print(row)
+    print("\n")
 
     """
     Слово w кодируются так:
-    w = x * 2^k + R, где
-      R = (x * 2^k) % polynom, т.е. остаток от деления на порождающий многочлен
-      x принимает значения от 1 до (2^m - 1) включительно
+    w = x * pow(2, k) + R, где
+      R - остаток от деления на многочлен, см polynom_remain()
+      x принимает значения от 1 до (pow(2, m) - 1) включительно
     При том расстояние между каждым кодовым словом w должно быть >= d, т.е.
     они должны отличаться друг от друга минимум d битами
     """
 
     coded = []
     for x in range(1, 2**m):
-        multiply = x * 2**k
-        r = multiply % polynom
-        coded.append(multiply + r)
+        r = polynom_remain(x << k, polynom)  # (x * 2**k) = (x << k)
+        code_word = (x << k) + r
+        coded.append(code_word)
 
     for i in coded:
         in_bin = str(bin(i)).lstrip("0b")
