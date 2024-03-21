4df0a4a4ef8c2a8d4aa059df6f561b367edb6f24
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-03-21 22:35:09 +0200

    lab 5 discrete math done

--- Discrete_Math/05_lab/cyclic_codes.py
+++ Discrete_Math/05_lab/cyclic_codes.py
@@ -33,6 +33,7 @@ def main():
 
     # Порождающая матрица имеет m строк и n столбцов, где в каждой строке
     # записывается полином в отражённом виде и сдвигается вправо каждую строку
+
     matrix = [[0]*n for i in range(0, m)]
     offset = 0
     for i in range(0, m):
@@ -56,6 +57,7 @@ def main():
     #   x принимает значения от 1 до (pow(2, m) - 1) включительно
     # При том расстояние между каждым кодовым словом w должно быть >= d, т.е.
     # они должны отличаться друг от друга минимум d битами
+
     coded = {}
     for x in range(1, 2**m):
         multiply = x << k  # (x * 2**k) = (x << k)
@@ -64,14 +66,13 @@ def main():
 
     # Определение минимального расстояния Хемминга:
     keys = list(coded.keys())
+    values = list(coded.values())
     d_min = float("inf")
     for i in range(len(keys)):
         current = keys[i]
         for j in range(i+1, len(keys)):
             other = keys[j]
-
             d = to_bin(coded[current][0] ^ coded[other][0]).count('1')
-
             coded[current][1] = min(coded[current][1], d)
             coded[other][1] = min(coded[other][1], d)
             d_min = min(d_min, d)
@@ -92,8 +93,28 @@ def main():
     error_vector = 0b001010000000001
     print("\nВозьмём вектор ошибки:", leading_0(error_vector, n))
 
-    syndrome = polynom_remain(coded[2][0] ^ error_vector, polynom)
-    print("Синдром этого вектора:", to_bin(syndrome))
+    wrong_code = coded[1][0] ^ error_vector
+    syndrome = polynom_remain(wrong_code, polynom)
+    print("\nСиндром этого вектора:", to_bin(syndrome))
+
+    # Чтобы найти все возможные вектора ошибок с одинаковым синдромом, нужно:
+    #   1) вектор ошибок E1 поделить (как многочлены) на кодовое слово X1
+    #   2) получившийся результат поделить на другое кодовое слово X2
+    #   3) результат из шага 2 снова поделить на кодовое слово X2, получим E2
+    #   4) проделать шаги 2 и 3 для всех слов, записывая все E в список
+    #   5) получившийся в итоге список и будет ответом
+
+    error_vectors = [error_vector]
+    for i in range(1, len(values)):
+        current = values[i][0]
+        wrong_code = wrong_code ^ current
+        new_error_vector = wrong_code ^ current
+        if new_error_vector not in error_vectors:
+            error_vectors.append(new_error_vector)
+
+    print("\nДругие вектора ошибок с этим же синдромом:")
+    for i in range(1, len(error_vectors)):
+        print(leading_0(error_vectors[i], n))
 
     print("\nКратность гарантированно обнаружаемых ошибок:", d_min - 1)
 
@@ -101,7 +122,7 @@ def main():
     # что они полностью заменяют одно кодовое слово на другое
     # Эти вектора совпадают с кодовыми словами
     print("\nВектора ошибок, которые не могут быть обнаружены:")
-    for i in list(coded.values()):
+    for i in values:
         print(leading_0(i[0], n))
 
 
