490cb7d73afaad9ea582c6cf4b40ed9777781edc
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-05-24 11:26:19 +0200

    task 17 algos finished

--- Algorithms/4th_Semester/17_task/crates.py
+++ Algorithms/4th_Semester/17_task/crates.py
@@ -2,16 +2,52 @@
 Имеется N предметов различного размера. Один ящик имеет строгую вместимость. Необходимо разложить все N предметов в минимальное количество ящиков.
 """
 
+
 def main():
     capacity = float(input("Введите вместимость одного ящика: "))
-    items = []
+    items, crates = [], []
 
     print("Введите веса предметов:")
     weight = input()
     while weight != "":
-        items.append(float(weight))
+        temp = float(weight)
+        if temp > capacity:
+            print("Этот предмет не влезет ни в один ящик. Введите другой:")
+        else:
+            items.append(temp)
         weight = input()
+    
+    # Алгоритм наилучшего остатка
+    for item in items:
+        potential_crates = []
+        
+        # Суть в том, чтобы после добавления предмета в яшике оставалось как можно меньше места
+        # Для этого создаём сначала массив ящиков в которых предмет вообще может поместиться
+        for i in range(len(crates)):
+            if item <= crates[i]:
+                potential_crates.append(i)
+        
+        # Если такого ящика не оказалось, берём новый ящик и сразу ложим в него предмет
+        if not potential_crates:
+            crates.append(capacity - item)
+        
+        # Если же нашлись такие ящики, то кладём предмет в тот, в котором после этого останется как можно меньше места
+        else:
+            best_fit, max_cap = 0, capacity
 
+            for i in potential_crates:
+                if (crates[i] - item) < max_cap:
+                    max_cap = crates[i] - item
+                    best_fit = i
+
+            crates[best_fit] -= item
+
+    if crates:
+        print("Распределение предметов по ящикам:")
+        for i in range(len(crates)):
+            print(f"Ящик {i+1}: вес = {capacity - crates[i]}")
+
+    print("Всего ящиков =", len(crates))
 
 
 if __name__ == "__main__":
