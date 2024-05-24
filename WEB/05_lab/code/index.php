55a827c38d44e6b771d9607cc262aa8272928ec0
Rodeffs <guideandplaygamespilex24@gmail.com>
2024-05-24 23:48:48 +0200

    initialisation

---
+++ WEB/05_lab/code/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Working with files (painfully)</title>
+	</head>
+	<body>
+		<div id="submission">
+			<h1>Enter submission</h1>
+			<form method="post" action="submit.php">
+				<label for="email">Email</label>
+				<input type="email" name="email" required><br>
+
+				<label for="category">Category</label>
+				<select name="category" required>
+					<option value="business">Business</option>
+					<option value="education">Education</option>
+					<option value="finances">Finances</option>
+					<option value="family">Family</option>
+					<option value="other">Other</option>
+				</select><br>
+
+				<label for="title">Title</label>
+				<input type="title" name="title" required><br>
+
+				<label for="content">Content</label>
+				<textarea id="content" name="content" rows="20" cols="120" required></textarea><br>
+
+				<input type="submit" name="submit_broadcast" value="SUBMIT" >
+			</form>
+		</div>
+		<div id="table">
+			<h3>Uploaded submissions:</h3>
+			<table>
+				<thead>
+					<th>Category</th>
+					<th>Email</th>
+					<th>Title</th>
+					<th>Content</th>	
+				</thead>
+				<tbody>
+					<?php
+						function output($filePath, $toOutput) { // рекурсивное считывание
+							$directories = array_slice(scandir($filePath), 2); // т.к. первые два элемента - это . и ..
+
+							foreach ($directories as $dir) {
+								$nextFilePath = $filePath."/{$dir}";
+								
+								if (mime_content_type($nextFilePath) == "text/plain") { // проверка на текстовый файл
+									$title = substr($dir, 0, -4); // чтобы убрать .txt с конца
+									$text = nl2br(file_get_contents($nextFilePath)); // nl2br нужно, чтобы правильно отображать \n
+
+									echo "{$toOutput}<td>{$title}</td><td>{$text}</td></tr>";
+								}
+								else {
+									output($nextFilePath, $toOutput."<td>{$dir}</td>");
+								}	
+							}
+						}
+						
+						if (is_dir("categories")) {
+							output("categories", "<tr>");
+						}
+					?>
+				</tbody>	
+			</table>
+		</div>
+	</body>
+</html>
---
+++ WEB/05_lab/code/submit.php
@@ -0,0 +1,51 @@
+<?php
+
+function goBack() {
+	header("Location: /");
+	exit();
+}
+
+if (false === isset($_POST["email"], $_POST["category"], $_POST["title"], $_POST["content"]))  {
+	goBack();
+}
+
+$email = $_POST["email"];
+$category = $_POST["category"];
+$title = $_POST["title"];
+$content = $_POST["content"];
+
+
+
+$filePath = "";
+$directories = ["categories", "/{$category}", "/{$email}"];
+
+foreach ($directories as $dir) {
+	$filePath.=$dir;
+	if (! is_dir($filePath)) {
+		mkdir($filePath); // почему-то mkdir() не изменяет permissions на 0777, хотя должен
+		chmod($filePath, 0777);
+	}
+}
+
+$filePath.="/{$title}.txt";
+
+if (false === file_put_contents($filePath, $content)) {
+	throw new Exception("Error while writing content to a file");
+}
+
+// File permissions in Linux:
+// 	First digit - owner
+// 	Second digit - group
+// 	Third digit - others
+// To come up with a digit for each category you need to sum up the following numbers:
+// 	4 - for read access
+// 	2 - for write access
+// 	1 - for execute access
+// E.g. if I need a file that the owner can read, write and execute, the group can only read and execute and all others can't do anything, then I write:
+// 	chmod 750 file
+
+chmod($filePath, 0777);
+
+goBack();
+
+?>
